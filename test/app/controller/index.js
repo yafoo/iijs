3aac80578318c4a10d9beb60c1b4f657e336e016
雨思 <yafu>
2019-11-23 00:27:29 +0800

    版本更新到2.3.2，完善db类where查询，model类添加连接、主键属性

--- test/app/controller/index.js
+++ test/app/controller/index.js
@@ -18,7 +18,7 @@ class Index extends Controller {
 
     async mysql() {
         const db = new Db(this.ctx);
-        const list = await db.table('article a').prefix('iime_').join('cate c', 'c.id=a.cate_id').field('a.title, a.id, a.click, c.cate_title').where({'a.click': 200}).order('a.id', 'desc').limit(1, 2).select();
+        const list = await db.table('article a').field('a.title, a.id, a.click, c.cate_title').join('cate c', 'c.id=a.cate_id').where({'a.click': ['in', '102,201'], source: ['=', 'me', 'or']}).where({add_time: 2, update_time: 0}, 'or').where({add_time: ['>=', 0], update_time: 0}).group('add_time').having('add_time>1').order('a.id', 'desc').limit(0, 10).select();
 
         const Arc = require('../model/article');
         const model_article = new Arc(this.ctx, this.next);
@@ -28,13 +28,6 @@ class Index extends Controller {
         const data = {"cate_id":2,"user_id":0,"title":"测试文章","writer":"雨思","source":"me","source_link":"","click":200,"keywords":"测试,文章","description":"这是一篇测试文章","content":"测试文章测试'文章测试文章内容"};
         const data2 = {"cate_id":2,"user_id":0,"title":"rtrtrt","writer":"雨思","source":"me","source_link":"","click":200,"keywords":"测试,文章","description":"这是一篇测试文章","content":"test'cccccc"};
         console.log(await model_article.db.table('article').sql().insert(data));
-
-        // const data = {"cate_id":2,"user_id":0,"title":"测试文章","writer":"雨思","source":"me","source_link":"","click":200,"keywords":"测试,文章","description":"这是一篇测试文章","content":"测试文章测试文章测试文章内容"};
-        // const data2 = {"cate_id":2,"user_id":0,"title":"测试文章","writer":"雨思","source":"me","source_link":"","click":200,"keywords":"测试,文章","description":"这是一篇测试文章","content":"测试文章测试文'章测试文章内容"};
-        // await model_article.db.startTrans(async () => {
-        //     await model_article.db.insert(data);
-        //     await model_article.db.insert(data2);
-        // });
         
 
         this.ctx.body = {list, list2, list3, list4};
