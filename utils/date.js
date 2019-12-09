eafa8902ff8edc3dadd082cb9957dd1beefa823a
雨思 <yafu>
2019-12-09 12:00:56 +0800

    日期格式化工具修复

--- utils/date.js
+++ utils/date.js
@@ -1,14 +1,15 @@
 function format(fmt, date) {
-    (typeof date === 'string' || typeof date === 'number') && (date = new Date(date + '000'));
+    (typeof date === 'string' || typeof date === 'number') && (date = new Date(parseInt(date + '000')));
 	  date = date || new Date();
     let ret;
     let opt = {
         "Y+": date.getFullYear().toString(),        // 年
+        "y+": date.getFullYear().toString().slice(2),// 年
         "m+": (date.getMonth() + 1).toString(),     // 月
         "d+": date.getDate().toString(),            // 日
-        "H+": date.getHours().toString(),           // 时
-        "M+": date.getMinutes().toString(),         // 分
-        "S+": date.getSeconds().toString()          // 秒
+        "h+": date.getHours().toString(),           // 时
+        "i+": date.getMinutes().toString(),         // 分
+        "s+": date.getSeconds().toString()          // 秒
     };
     for (let k in opt) {
         ret = new RegExp("(" + k + ")").exec(fmt);
