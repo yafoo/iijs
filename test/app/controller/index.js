a36fbcffe999b542dec9b9e43cb30b70af8e0afe
雨思 <yafu>
2019-12-10 13:49:45 +0800

    新增数据库操作类并发读取

--- test/app/controller/index.js
+++ test/app/controller/index.js
@@ -17,19 +17,24 @@ class Index extends Controller {
     }
 
     async mysql() {
-        const db = new Db(this.ctx);await db.startTrans();
+        const db = new Db(this.ctx);
         const list = await db.table('article a').field('a.title, a.id, a.click, c.cate_name').join('cate c', 'c.id=a.cate_id').where({'a.click': ['in', '102,201'], source: ['=', 'me', 'or']}).where({add_time: 2, update_time: 0}, 'or').where({add_time: ['>=', 0], update_time: 0}).group('add_time').having('add_time>1').order('a.id', 'desc').limit(0, 10).select();
 
         const Arc = require('../model/article');
         const model_article = new Arc(this.ctx, this.next);
-        const list2 = await model_article.db.find();
-        const list3 = await this.ctx.$ii.app.model.article.db.find();
-        const list4 = await this.ctx.$ii.app.model.article.db.page(2, 3).select();
-        const data = {"cate_id":2,"user_id":0,"title":"测试文章","writer":"雨思","source":"me","source_link":"","click":200,"keywords":"测试,文章","description":"这是一篇测试文章","content":"测试文章测试'文章测试文章内容"};
-        const data2 = {"cate_id":2,"user_id":0,"title":"rtrtrt","writer":"雨思","source":"me","source_link":"","click":200,"keywords":"测试,文章","description":"这是一篇测试文章","content":"test'cccccc"};
+        const [list2, list3, list4] = await Promise.all([
+            model_article.db.find(),
+            this.ctx.$ii.app.model.article.db.find(),
+            this.ctx.$ii.app.model.article.db.page(2, 3).select()
+        ]);
+        //const list2 = model_article.db.find();
+        //const list3 = this.ctx.$ii.app.model.article.db.find();
+        //const list4 = this.ctx.$ii.app.model.article.db.page(2, 3).select();
+        //const data = {"cate_id":2,"user_id":0,"title":"测试文章","writer":"雨思","source":"me","source_link":"","click":200,"keywords":"测试,文章","description":"这是一篇测试文章","content":"测试文章测试'文章测试文章内容"};
+        //const data2 = {"cate_id":2,"user_id":0,"title":"rtrtrt","writer":"雨思","source":"me","source_link":"","click":200,"keywords":"测试,文章","description":"这是一篇测试文章","content":"test'cccccc"};
         //console.log(await model_article.db.table('article').sql().insert(data));
         
-db.commit();
+
         this.ctx.body = {list, list2, list3, list4};
     }
 }
