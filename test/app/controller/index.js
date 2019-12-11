c44b22169e6dbdd7be739cbb55ef50192e4fb8a1
雨思 <yafu>
2019-12-11 18:38:06 +0800

    新增ctx类，方便模型、逻辑调用

--- test/app/controller/index.js
+++ test/app/controller/index.js
@@ -13,6 +13,14 @@ class Index extends Controller {
     }
 
     async hello() {
+        console.log(this.$root);
+        console.log(this.$model);
+        console.log(this.$model.article);
+        console.log(this.$model.article === this.$model.article);
+        console.log(this.$model.article.db === this.$model.article.db);
+        console.log(this.$app);
+        console.log(this.$config);
+        console.log(this.$config.app);
         await this.display(`<div style="font-size:50px;">hello iijs, hello world !</div>`);
     }
 
@@ -24,12 +32,12 @@ class Index extends Controller {
         const model_article = new Arc(this.ctx, this.next);
         const [list2, list3, list4] = await Promise.all([
             model_article.db.find(),
-            this.ctx.$ii.app.model.article.db.find(),
-            this.ctx.$ii.app.model.article.db.page(2, 3).select()
+            this.ctx.$iijs.app.model.article.db.find(),
+            this.ctx.$iijs.app.model.article.db.page(2, 3).select()
         ]);
         //const list2 = model_article.db.find();
-        //const list3 = this.ctx.$ii.app.model.article.db.find();
-        //const list4 = this.ctx.$ii.app.model.article.db.page(2, 3).select();
+        //const list3 = this.ctx.$iijs.app.model.article.db.find();
+        //const list4 = this.ctx.$iijs.app.model.article.db.page(2, 3).select();
         //const data = {"cate_id":2,"user_id":0,"title":"测试文章","writer":"雨思","source":"me","source_link":"","click":200,"keywords":"测试,文章","description":"这是一篇测试文章","content":"测试文章测试'文章测试文章内容"};
         //const data2 = {"cate_id":2,"user_id":0,"title":"rtrtrt","writer":"雨思","source":"me","source_link":"","click":200,"keywords":"测试,文章","description":"这是一篇测试文章","content":"test'cccccc"};
         //console.log(await model_article.db.table('article').sql().insert(data));
