5164b1dc500100e314382ad1d3aba480321ff770
雨思 <yafu>
2019-12-13 14:57:51 +0800

    优化路由，支持动态path，增加空控制器、空方法，及前置后置方法

--- test/config/route.js
+++ test/config/route.js
@@ -1,6 +1,8 @@
 route = [
     {url: '/hello', path: 'index/hello', method: 'get'},
     {url: '/mysql', path: 'index/mysql', method: 'get'},
+    {url: '/file', path: 'index/index', method: 'get', type: 'view'},
+    {url: '/:mysql', path: 'index/${mysql}', method: 'get'},
 ];
 
 module.exports = route;
