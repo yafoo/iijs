98af1bcfc1cc6e48c84fee40e13a69a2926dbe4f
yafu <zhyaphoo@qq.com>
2020-01-09 14:46:44 +0800

    控制器流程控制

--- test/app/controller/cate.js
+++ test/app/controller/cate.js
@@ -1,11 +1,23 @@
 const {Controller, Cookie} = require('../../../iijs');
 class Cate extends Controller {
+    async _init() {
+        console.log('_init');
+    }
+
+    async index() {
+        this.display('cate index');
+    }
+
     async cate() {
         const cookie = new Cookie(this.ctx, this.next);
         cookie.set('test', 'value');
         cookie.set('test2', 'value2', {maxAge: 1000 * 3600});
         cookie.set('test4', 'value4', {expires: new Date('2020-07-06')});
-        this.display(cookie.all('test'));
+        this.display(cookie.all());
+    }
+
+    async _end() {
+        console.log('_end');
     }
 }
 
