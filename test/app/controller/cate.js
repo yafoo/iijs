d98bc0483640f16c1c4fbd1d10ecb61f6fee3ef0
www <www@i-i.me>
2020-01-10 18:38:41 +0800

    新增url生成类

--- test/app/controller/cate.js
+++ test/app/controller/cate.js
@@ -1,4 +1,4 @@
-const {Controller, Cookie} = require('../../../iijs');
+const {Controller, Cookie, Url} = require('../../../iijs');
 class Cate extends Controller {
     async _init() {
         console.log('_init');
@@ -14,6 +14,26 @@ class Cate extends Controller {
         cookie.set('test2', 'value2', {maxAge: 1000 * 3600});
         cookie.set('test4', 'value4', {expires: new Date('2020-07-06')});
         this.display(cookie.all());
+    }
+
+    async url() {
+        const url =  new Url(this.ctx, this.next);
+        const arr = [];
+        arr.push(url.build());
+        arr.push(url.build('cate', '/'));
+        arr.push(url.build('index/cate', '.html'));
+        arr.push(url.build('index/index/cate', '.html'));
+        arr.push(url.build('app/index/index/cate', '.html'));
+        arr.push(url.build('/app/index/index/cate', '.html'));
+        arr.push(url.build('test', {var: 'test', var2: 'test2'}, '.html'));
+        arr.push(url.build('test?var3=vvv', {var: 'test', var2: 'test2'}, '.html'));
+        arr.push(url.build('test?var=vvv', {var: 'test', var2: 'test2'}, '.html'));
+        arr.push(url.build('index/cate#'));
+        arr.push(url.build('index/cate#', {var: 'test', var2: 'test2'}, '.html'));
+        arr.push(url.build('test?var=vvv#bbb', {var: 'test', var2: 'test2'}, '.html'));
+        arr.push(url.build(':name', {var: 'test', var2: 'test2', var3: 'test3'}, '.html'));
+
+        this.display(arr.join('\n'));
     }
 
     async _end() {
--- test/config/routes.js
+++ test/config/routes.js
@@ -4,6 +4,7 @@ routes = [
     {url: '/hello/list_:page.html*', path: 'index/hello', method: 'get'},
     {url: '/mysql', path: 'index/mysql', method: 'get'},
     {url: '/file', path: 'index/index', method: 'get', type: 'view'},
+    {url: '/:test/:var2/:var', path: 'index/index', method: 'get', type: 'view', name: 'name'},
     // {url: '/:mysql', path: 'index/${mysql}', method: 'get'},
 ];
 
