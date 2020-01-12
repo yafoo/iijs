fa48c0a2c37265a411febebc6cc789aa1ccc14f2
yusi <zhyaphoo@qq.com>
2020-01-12 12:06:21 +0800

    utils目录移动

--- iijs.js
+++ iijs.js
@@ -3,7 +3,6 @@ module.exports = new Proxy({}, {
         if(prop in target || typeof prop == 'symbol' || prop == 'inspect'){
             return target[prop];
         }
-        const folder = prop == 'utils' ? 'utils' : 'lib';
-        return require('./' + folder + '/' + prop.toLowerCase());
+        return require('./lib/' + prop.toLowerCase());
     }
 });
